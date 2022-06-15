d394aeee90dfb65d55b2811846a9025f251f1ab3
NursultanSakiev <100749627+NursultanSakiev@users.noreply.github.com>
2022-06-16 00:57:45 +0600

    Initial commit

---
+++ app/src/main/java/ProfileFragment/ProfileFragment.kt
@@ -0,0 +1,39 @@
+package ProfileFragment
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.result.contract.ActivityResultContracts
+import com.example.newsapp.R
+import com.example.newsapp.databinding.FragmentProfileBinding
+
+
+class ProfileFragment : Fragment() {
+    private lateinit var binding: FragmentProfileBinding
+    val getContent = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.imageView.setOnClickListener {
+            getContent.launch("image/*")
+        }
+    }
+
+
+}
---
+++ app/src/main/java/com/example/newsapp/NewsFragment.kt
@@ -0,0 +1,40 @@
+package com.example.newsapp
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.newsapp.databinding.FragmentNewsBinding
+
+
+class NewsFragment : Fragment() {
+    private lateinit var binding: FragmentNewsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentNewsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSave.setOnClickListener {
+            save()
+        }
+    }
+
+    private fun save() {
+        val text = binding.editText.text.toString().trim()
+        val bundle = Bundle()
+        bundle.putString("text", text)
+        parentFragmentManager.setFragmentResult("rk_news", bundle)
+        findNavController().navigateUp()
+    }
+
+}
